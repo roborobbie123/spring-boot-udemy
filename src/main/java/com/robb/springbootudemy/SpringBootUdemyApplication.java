c8320f737f23e635779716ad7a2e2921af17fe1c
Robert Barrett <roborobbie123@gmail.com>
2025-01-30 13:02:44 -0500

    initial commit

---
+++ src/main/java/com/robb/springbootudemy/SpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.robb.springbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robb/springbootudemy/SpringBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robb.springbootudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
